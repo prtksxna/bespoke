cda4615af96d736a2545de42ed4a5e7787f30469
Prateek Saxena <prtksxna@gmail.com>
2008-10-13 00:07:38 +0530

    Added Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
+  map.resource :session
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
@@ -30,6 +34,11 @@ ActionController::Routing::Routes.draw do |map|
   # See how all your routes lay out with "rake routes"
 
   # Install the default routes as the lowest priority.
+
+	map.signup '/signup', :controller => 'users', :action => 'new'
+	map.login '/login', :controller => 'sessions', :action => 'new'
+	map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
