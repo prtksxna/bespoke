a7fc05ca71b89a7a3819de9071c26b39a8d70199
Prateek Saxena <prtksxna@gmail.com>
2009-05-11 16:43:22 +0530

    Fresh start

--- db/migrate/002_create_organization.rb
+++
@@ -1,9 +0,0 @@
-class CreateOrganization < ActiveRecord::Migration
-  def self.up
-		add_column :users, :org, :string
-  end
-
-  def self.down
-		remove_column :users, :org
-  end
-end
--- db/migrate/003_create_url_org_user.rb
+++
@@ -1,9 +0,0 @@
-class CreateUrlOrgUser < ActiveRecord::Migration
-  def self.up
-		add_column :users, :url, :string
-  end
-
-  def self.down
-		remove_column :users, :url
-  end
-end
